3cdf07cf8fa79e3fb6a504f732b1dd01de1bf5d0
StartForKiller <jesussanz2003@gmail.com>
2018-03-19 16:56:53 +0100

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -6,7 +6,7 @@ module.exports = class extends Event {
     });
   }
   run(member) {
-    const channel = member.guild.channels.find("name", "actividad");
+    const channel = member.guild.channels.get('405850852411179040');
     if (!channel) return;
     channel.send(
       "```fix\n¡AHOY PIRATA!\n```\n<:barco:406838651771682818> **¡Un nuevo barco acaba de atracar en el puerto!**\n\n_El pirata" + `${member}` + "entra de un portazo en la <#375828283704475649> sediento de una buena jarra de grog_ <:jarra:406906694766034947>\n\nhttp://gph.is/2Gd8vlD\n"
