789d0352052c4318bde6b68800dfd8856895ed6d
StartForKiller <jesussanz2003@gmail.com>
2018-03-19 16:56:28 +0100

    Update guildBanAdd.js

--- events/guildBanAdd.js
+++ events/guildBanAdd.js
@@ -6,7 +6,7 @@ module.exports = class extends Event {
     });
   }
   run(member) {
-    const channel = member.guild.channels.find("name", "actividad");
+    const channel = member.guild.channels.get('405850852411179040');
     if (!channel) return;
     channel.send("```fix\nPASEÓ POR LA PLANCHA\n```\n<:barco:406838651771682818> **¡Alto ahí! Más os vale saltar por la borda**\n\n_El pirata" + `${member}` + "es condenado a criar malvas devorado por los tiburones por traidor. Sus restos son recuperados y usados para decorar la {#taberna}._ 🦈\n\nhttp://gph.is/2Ged67e\n");
   }
