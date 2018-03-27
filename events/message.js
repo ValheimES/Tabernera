37e2db6d67aa10c1759ffdadf918c1c5ec859a23
StartForKiller <jesussanz2003@gmail.com>
2018-03-27 20:37:17 +0200

    Añadido el sistema de experiencia
    Hay que eliminar el evento de Klasa para que este funcione, en algunos casos.

---
+++ events/message.js
@@ -0,0 +1,65 @@
+const { Event } = require('klasa');
+
+
+module.exports = class extends Event {
+
+	async run(msg) {
+const MySql = await this.client.providers.get('MySQL');
+		if (msg.channel.name != "comandos")
+		{
+			const limpia = '```fix\nSISTEMA DE NIVELES\n```\n<:flechaarriba:406932788256702474> **' + msg.author + ' ha subido de nivel.**\n\n<:garfio:407239812873977857> **Nivel ';
+			const canal = msg.guild.channels.get('425298298446807041');
+			const exists = await MySql.has3("XP", `${msg.author.id}`);
+			if(exists) {
+				const base = await MySql.get("XP", "ID", "" + msg.author.id);
+				var xp = Math.round(Math.random())
+				if (xp == 0) {
+					xp = 10;
+				} else {
+					xp = 15;
+				}
+				if(MySql.update4("XP", "" + msg.author.id, "XP", "" + String(parseInt(base.XP) + xp)))
+				{
+					const base2 = await MySql.get("XP", "ID", "" + msg.author.id);
+						if(parseInt(base2.XP) >= 23850 && parseInt(base2.XP) >= 67525 && !msg.member.roles.exists('id', '424585537035304962'))
+						{
+							msg.member.addRole('424585537035304962'); // Rey pirata
+							msg.member.removeRole('424585482119413760'); // Pirata experto
+							canal.send(limpia + '6:** Rey pirata.');
+						}
+						else if(parseInt(base2.XP) >= 11825 && parseInt(base2.XP) >= 23850 && !msg.member.roles.exists('id', '424585482119413760'))
+						{
+							msg.member.addRole('424585482119413760'); // Pirata experto
+							msg.member.removeRole('424585387596447765'); // Pirata rufián
+							canal.send(limpia + '5:** Pirata experto.');
+						}
+						else if(parseInt(base2.XP) >= 4675 && parseInt(base2.XP) >= 11825 && !msg.member.roles.exists('id', '424585387596447765'))
+						{
+							msg.member.addRole('424585387596447765'); // Pirata rufián
+							msg.member.removeRole('424585289852256268'); // Grumete Gallina
+							canal.send(limpia + '4:** Pirata rufián.');
+						}
+						else if(parseInt(base2.XP) >= 1150 && parseInt(base2.XP) >= 4675 && !msg.member.roles.exists('id', '424585289852256268'))
+						{
+							msg.member.addRole('424585289852256268'); // Grumete Gallina
+							msg.member.removeRole('424585155408166922'); // Rata de Mar
+							canal.send(limpia + '3:** Grumete gallina.');
+						}
+						else if(parseInt(base2.XP) >= 1 && parseInt(base2.XP) >= 1150  && !msg.member.roles.exists('id', '424585155408166922'))
+						{
+							msg.member.addRole('424585155408166922'); // Rata de Mar
+							msg.member.removeRole('424585008984883203'); // Limpiacubiertas
+							canal.send(limpia + '2:** Rata de mar.');
+						}
+				}
+			}
+			else {
+				await MySql.insert2("XP", ["ID", "XP"], [("" + msg.author.id), "0"]);
+				msg.member.addRole('424585008984883203'); // Limpiacubiertas
+				canal.send(limpia + '1:** Limpiacubiertas.');
+}
+}
+		if (this.client.ready) this.client.monitors.run(msg);
+	}
+
+};
