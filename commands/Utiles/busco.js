8e2db20ea0277aa6799b1c5ee44e29b91d428b0e
StartForKiller <jesussanz2003@gmail.com>
2018-04-05 18:09:57 +0200

    Corregido errores y bugs varios

--- commands/Utiles/busco.js
+++ commands/Utiles/busco.js
@@ -1,41 +1,47 @@
-const Comando = require("../../estructuras/Comando");
+const Comando = require('../../estructuras/Comando');
 const Discord = require('discord.js');
+
+var urlBarco;
 
 module.exports = class extends Comando {
 
-constructor(...args) {
-        super(...args, {
-	    permlevel: 3,
-            cooldown: 15,
-	    requiredSettings: ['busco'],
-            description: "Por añadir",
-            extendedHelp: "+busco",
-            comando: "+busco",
-            });
-    }
+	constructor(...args) {
+		super(...args, {
+			permlevel: 3,
+			cooldown: 15,
+			requiredSettings: ['busco'],
+			description: 'Por añadir',
+			extendedHelp: '+busco',
+			comando: '+busco'
+		});
+	}
 
-    async run(msg) {
-	const voiceChannel = msg.member.voiceChannel;
-	if(!voiceChannel)
-		return msg.send('**' + msg.author + ' debes conectarte a un barco para pedir tripulación.** 🚢');
-	if(msg.guid !== voiceChannel.guid)
-		return msg.send('**' + msg.author + ' debes conectarte a un barco para pedir tripulación.** 🚢');
-	if(voiceChannel.full)
-		return msg.send('**' + msg.author + ' no puedes pedir más tripulantes ¡tu barco ya está lleno!** 🚫');
+	async run(msg) {
+		const voiceChannel = msg.member.voiceChannel;
+		if (!voiceChannel) { return msg.send(`**${msg.author} debes conectarte a un barco para pedir tripulación.** 🚢`); }
+		if (msg.guid !== voiceChannel.guid) { return msg.send(`**${msg.author} debes conectarte a un barco para pedir tripulación.** 🚢`); }
+		if (voiceChannel.full) { return msg.send(`**${msg.author} no puedes pedir más tripulantes ¡tu barco ya está lleno!** 🚫`); }
 
-	const usuariosNecesarios = voiceChannel.userLimit - voiceChannel.members.array().length;
-	const canal = msg.guild.channels.get(msg.guild.configs.busco);
-	
-	const embedBarco = new Discord.MessageEmbed()
-                .setColor(0x00ced1)
-                .addField(`_Busco **${usuariosNecesarios}** ${(usuariosNecesarios === 1) ? 'pirata' : 'piratas'} en el barco ${voiceChannel.name} para zarpar._ 🚢`);
+		const usuariosNecesarios = voiceChannel.userLimit - voiceChannel.members.array().length;
+		const canal = msg.guild.channels.get(msg.guild.configs.busco);
 
-	canal.send('**' + msg.author + ' dice:**')
-	canal.send(embedBarco);	
-	canal.send('[<@&430418605423853568>]');
-	voiceChannel.createInvite().then(invite => canal.send(invite.url));	
-	
-	return true;
-    }
+		await voiceChannel.createInvite().then(invite => urlset(invite.url));
+
+		const embedBarco = new Discord.MessageEmbed()
+			.setTitle('Click aqui para zarpar')
+			.setURL(urlBarco)
+			.setColor(0x00ced1)
+			.addField(`_Busco **${usuariosNecesarios}** ${usuariosNecesarios === 1 ? 'pirata' : 'piratas'} en el barco ${voiceChannel.name} para zarpar._`, 'Embarcate !!!');
+
+		canal.send(`**${msg.author} dice:**`);
+		canal.send(embedBarco);
+		canal.send('[<@&430418605423853568>]');
+
+		return true;
+	}
 
 };
+
+async function urlset(url) {
+	urlBarco = url;
+}
--- commands/Utiles/estado.js
+++ commands/Utiles/estado.js
@@ -2,29 +2,30 @@ const Comando = require('../../estructuras/Comando');
 
 module.exports = class extends Comando {
 
-    constructor(...args) {
-        super(...args, {
-            name: 'estado',
-            enabled: true,
-            runIn: ['text'],
-            cooldown: 4,
-            description: 'Cambia el estado.',
-            usage: '<estado:str> [...]',
-            extendedHelp: '+estado',
-            comando: '+estado'
-        });
-    }
-    async run(msg, [...estado]) {
-          const bot = msg.client.users.get("375079939029991425");
+	constructor(...args) {
+		super(...args, {
+			name: 'estado',
+			enabled: true,
+			runIn: ['text'],
+			cooldown: 4,
+			permLevel: 6,
+			description: 'Cambia el estado.',
+			usage: '<estado:str> [...]',
+			extendedHelp: '+estado',
+			comando: '+estado'
+		});
+	}
+	async run(msg, [...estado]) {
+		const bot = msg.client.users.get('408643206369116160');
 
-          estado = `${estado.join(' ')}`;
+		estado = `${estado.join(' ')}`;
 
-          await bot.setActivity(estado);
-			
-            await msg.channel.send("Se ha cambiado el estado.");
-			await msg.delete(100);
+		await bot.setActivity(estado);
 
-          return true;
+		await msg.channel.send('Se ha cambiado el estado.');
+		await msg.delete(100);
 
-  }
+		return true;
+	}
+
 };
