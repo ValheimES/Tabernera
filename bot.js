6e0e2b805cd5a62765c9b6b32647dcc9935b1d1c
StartForKiller <jesussanz2003@gmail.com>
2018-04-13 18:36:39 +0200

    Añadido cambios bot.js

--- bot.js
+++ bot.js
@@ -3,10 +3,11 @@ const config = require('./config.json');
 const { Client } = require('klasa');
 
 Client.defaultPermissionLevels
-	.addLevel(3, false, (client, msg) => msg.member.roles.exists('name', 'Usuario') || msg.member.roles.exists('name', 'Vieja escuela'))
+	.addLevel(3, false, (client, msg) => msg.member.roles.exists('id', '397323241846341632') || msg.member.roles.exists('name', 'Vieja escuela'))
 	.addLevel(4, false, (client, msg) => msg.member.roles.exists('name', 'Verificador'))
 	.addLevel(5, false, (client, msg) => msg.member.roles.exists('name', 'Moderador'))
-	.addLevel(6, false, (client, msg) => msg.member.roles.exists('name', 'Administrador'));
+	.addLevel(6, false, (client, msg) => msg.member.roles.exists('name', 'Administrador'))
+	.addLevel(10, false, (client, msg) => msg.member.id === '207164528222404608');
 
 const cliente = new klasa.Client({
 	clientOptions: {
