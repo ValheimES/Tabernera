0e180621274ffbae3865630cbffc8c29ec53dc92
minikanyas <minikanyas@gmail.com>
2018-04-15 01:38:08 +0200

    Optimización de uso de la API en el monitor nivel

--- monitors/nivel.js
+++ monitors/nivel.js
@@ -1,4 +1,5 @@
 const { Monitor } = require('klasa');
+const { Collection } = require('discord.js');
 
 module.exports = class extends Monitor {
 
@@ -25,24 +26,19 @@ module.exports = class extends Monitor {
 				if (MySql.update4('XP', `${msg.author.id}`, 'XP', `${String(parseInt(base.XP) + xp)}`)) {
 					const base2 = await MySql.get('XP', 'ID', `${msg.author.id}`);
 					if (parseInt(base2.XP) >= 23851 && parseInt(base2.XP) >= 67525 && !msg.member.roles.exists('id', '424585537035304962')) {
-						msg.member.roles.add(await msg.guild.roles.find('id', '424585537035304962'));
-						setTimeout(() => { msg.member.roles.add(msg.guild.roles.find('id', '424585482119413760')); }, 1000);
+						await msg.member.roles.set(getRolesToSet(msg.member.roles, msg.guild.roles, ['424585537035304962'], ['424585482119413760']));
 						canal.send(`${limpia}6:** Rey pirata.`);
 					} else if (parseInt(base2.XP) >= 11825 && parseInt(base2.XP) >= 23850 && !msg.member.roles.exists('id', '424585482119413760') && !msg.member.roles.exists('id', '424585537035304962')) {
-						msg.member.roles.add(await msg.guild.roles.find('id', '424585482119413760'));
-						setTimeout(() => { msg.member.roles.remove(msg.guild.roles.find('id', '424585387596447765')); }, 1000);
+						await msg.member.roles.set(getRolesToSet(msg.member.roles, msg.guild.roles, ['424585482119413760'], ['424585387596447765']));
 						canal.send(`${limpia}5:** Pirata experto.`);
 					} else if (parseInt(base2.XP) >= 4676 && parseInt(base2.XP) >= 11825 && !msg.member.roles.exists('id', '424585387596447765') && !msg.member.roles.exists('id', '424585482119413760') && !msg.member.roles.exists('id', '424585537035304962')) {
-						msg.member.roles.add(await msg.guild.roles.find('id', '424585387596447765'));
-						setTimeout(() => { msg.member.roles.remove(msg.guild.roles.find('id', '424585289852256268')); }, 1000);
-						canal.send(`${limpia}4:** Pirata rufiÃ¡n.`);
+						await msg.member.roles.set(getRolesToSet(msg.member.roles, msg.guild.roles, ['424585387596447765'], ['424585289852256268']));
+						canal.send(`${limpia}4:** Pirata rufián.`);
 					} else if (parseInt(base2.XP) >= 1151 && parseInt(base2.XP) >= 4675 && !msg.member.roles.exists('id', '424585289852256268') && !msg.member.roles.exists('id', '424585387596447765') && !msg.member.roles.exists('id', '424585482119413760') && !msg.member.roles.exists('id', '424585537035304962')) {
-						msg.member.roles.add(await msg.guild.roles.find('id', '424585289852256268'));
-						setTimeout(() => { msg.member.roles.remove(msg.guild.roles.find('id', '424585155408166922')); }, 1000);
+						await msg.member.roles.set(getRolesToSet(msg.member.roles, msg.guild.roles, ['424585289852256268'], ['424585155408166922']));
 						canal.send(`${limpia}3:** Grumete gallina.`);
 					} else if (parseInt(base2.XP) >= 1 && parseInt(base2.XP) >= 1150 && !msg.member.roles.exists('id', '424585155408166922') && !msg.member.roles.exists('id', '424585289852256268') && !msg.member.roles.exists('id', '424585387596447765') && !msg.member.roles.exists('id', '424585482119413760') && !msg.member.roles.exists('id', '424585537035304962')) {
-						msg.member.roles.add(await msg.guild.roles.find('id', '424585155408166922'));
-						setTimeout(() => { msg.member.roles.remove(msg.guild.roles.find('id', '424585008984883203')); }, 1000);
+						await msg.member.roles.set(getRolesToSet(msg.member.roles, msg.guild.roles, ['424585155408166922'], ['424585008984883203']));
 						canal.send(`${limpia}2:** Rata de mar.`);
 					}
 				}
@@ -55,3 +51,24 @@ module.exports = class extends Monitor {
 	}
 
 };
+
+function getRolesToSet(serverRoles, memberRoles, rolesToAdd, rolesToDelete) {
+	var rolesToSet = new Collection;
+	memberRoles.forEach(element => {
+		rolesToSet.set(element.id, element);
+	});
+
+	if (rolesToAdd.length) {
+		rolesToAdd.forEach(element => {
+			rolesToSet.set(element, serverRoles.find('id', element));
+		});
+	}
+
+	if (rolesToDelete.length) {
+		rolesToDelete.forEach(element => {
+			rolesToSet.delete(element);
+		});
+	}
+
+	return rolesToSet;
+}
