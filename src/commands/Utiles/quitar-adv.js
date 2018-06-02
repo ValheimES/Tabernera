95025b5cf229d9583936c5117f3060f80814d2fa
kyranet <kyradiscord@gmail.com>
2018-06-02 21:23:21 +0200

    ¡Travis CI está alegre!

--- src/commands/Utiles/quitar-adv.js
+++ src/commands/Utiles/quitar-adv.js
@@ -1,90 +1,37 @@
 const Comando = require('../../estructuras/Comando');
 
-var var2;
-var Desc2;
-var Desc3;
-
 module.exports = class extends Comando {
-
 
 	constructor(...args) {
 		super(...args, {
-
-			name: 'quitar-adv',
-
 			permissionLevel: 5,
-
 			description: 'Elimina las advertencias de un usuario.',
-
-			usage: '<usuario:user> <numero:str>',
-
+			usage: '<usuario:user> <todos|numero:number{1,3}>',
 			extendedHelp: '+quitar-adv @Usuario 1',
-
 			comando: '+quitar-adv <Usuario> <Numero de advertencia>'
-
 		});
 	}
 
-
 	async run(msg, [usuario, numero]) {
-		const MySql = await this.client.providers.get('MySQL');
+		msg.delete(1000);
 
-
-		const exists = await MySql.has2('Strikes', `${usuario.id}`);
-
-
-		if (!exists) {
-			msg.delete(1000);
-
-			return msg.sendMessage('El usuario no tiene advertencias.');
-		} else {
-			const base = await MySql.get('Strikes', 'UserID', usuario.id);
-
-
-			if (base.Numero > numero && numero === '2' && base.Numero === '3') {
-				var2 = await MySql.get('Strikes', 'UserID', `${usuario.id}`);
-
-				Desc3 = var2.Desc3;
-
-
-				await MySql.update2('Strikes', `${usuario.id}`, 'Numero', `${2}`);
-				await MySql.update2('Strikes', `${usuario.id}`, 'Desc2', `${Desc3}`);
-				await MySql.update2('Strikes', `${usuario.id}`, 'Desc3', '');
-			} else if (base.Numero > numero && numero === '1' && base.Numero === '3') {
-				var2 = await MySql.get('Strikes', 'UserID', `${usuario.id}`);
-
-				Desc3 = var2.Desc3;
-
-				Desc2 = var2.Desc2;
-
-
-				await MySql.update2('Strikes', `${usuario.id}`, 'Numero', `${2}`);
-				await MySql.update2('Strikes', `${usuario.id}`, 'Desc1', `${Desc2}`);
-				await MySql.update2('Strikes', `${usuario.id}`, 'Desc2', `${Desc3}`);
-				await MySql.update2('Strikes', `${usuario.id}`, 'Desc3', '');
-			} else if (base.Numero === '3') {
-				MySql.update2('Strikes', `${usuario.id}`, 'Numero', `${2}`);
-				await MySql.update2('Strikes', `${usuario.id}`, 'Desc3', '');
-			} else if (base.Numero > numero && numero === '2' && base.Numero === '2') {
-				await MySql.update2('Strikes', `${usuario.id}`, 'Numero', `${1}`);
-				await MySql.update2('Strikes', `${usuario.id}`, 'Desc2', '');
-			} else if (base.Numero > numero && numero === '1' && base.Numero === '2') {
-				var2 = await MySql.get('Strikes', 'UserID', `${usuario.id}`);
-
-				Desc2 = var2.Desc2;
-
-
-				await MySql.update2('Strikes', `${usuario.id}`, 'Numero', `${1}`);
-				await MySql.update2('Strikes', `${usuario.id}`, 'Desc1', `${Desc2}`);
-			} else {
-				await MySql.delete3('Strikes', `${usuario.id}`);
-			}
-
-
-			msg.delete(1000);
-
-			return msg.sendMessage('Se ha eliminado la advertencia.');
+		const provider = this.client.providers.default;
+		const data = await provider.get('strikes', usuario.id);
+		if (!data) throw 'El usuario no tiene advertencias.';
+		if (numero === 'todos') {
+			await provider.delete('strikes', usuario.id);
+			return msg.sendMessage(`Se han eliminado ${data.numero} ${data.numero === '1' ? 'advertencia' : 'advertencias'}.`);
 		}
+
+		if (numero > data.numero)
+			throw `La advertencia ${numero} no existe.`;
+
+		const descripciones = [data.desc1, data.desc2, data.desc3];
+		descripciones.splice(numero, 1);
+		descripciones.push(null);
+		await provider.update('strikes', usuario.id, { numero: data.numero - 1, desc1: descripciones[0], desc2: descripciones[1], desc3: descripciones[2] });
+
+		return msg.sendMessage('Se ha eliminado la advertencia.');
 	}
 
 
