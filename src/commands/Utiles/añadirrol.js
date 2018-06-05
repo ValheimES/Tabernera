e7d36a8daa113843d3a09f64bdb0636bc74f9db0
StartForKillerMC <jesussanz2003@gmail.com>
2018-06-05 20:10:19 +0200

    Añadido el comando añadirrol

---
+++ src/commands/Utiles/añadirrol.js
@@ -0,0 +1,26 @@
+const { Command } = require('../../index');
+
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+			runIn: ['text'],
+			permissionLevel: 6,
+			usage: '<rol:role>',
+			description: 'Te permite incluír un rol en la lista de roles autoasignables',
+			extendedHelp: '+añadirrol @Meta 2000 piratas conseguida',
+			comando: '+añadir <Rol>'
+		});
+	}
+
+	async run(msg, [rol]) {
+		const { autoRoles } = msg.guild.configs.roles;
+
+		for (let i = 0; i < autoRoles.length; i++) {
+			if (autoRoles[i] === rol.id)
+				return msg.sendMessage('El rol ya fue añadido, revisa el diaro.');
+		}
+		return await msg.guild.configs.update('roles.autoRoles', rol.join(' '), msg.guild, { avoidUnconfigurable: true, action: 'add' });
+	}
+
+};
