5fa927a87e99a41e7cddc4326872e786170e626e
StartForKillerMC <jesussanz2003@gmail.com>
2018-07-31 10:26:35 +0200

    Segun lo que indica todo, deberia de estar acabado el sistema de twitch

--- src/commands/Utiles/twitch.js
+++ src/commands/Utiles/twitch.js
@@ -1,33 +1,53 @@
 const { Command } = require('../../index');
+const { MessageEmbed } = require('discord.js');
 
 module.exports = class extends Command {
 
 	constructor(...args) {
 		super(...args, {
-			name: 'perfil',
-			runIn: ['text', 'dm', 'group'],
-			permissionLevel: 3,
-			description: 'Mira el estado de tu cuenta.',
-			extendedHelp: '+twitch añadir/quitar <@usuario> <NombreCuentaTwitch>',
-			usage: '+twitch <añadir|quitar> <usuario:user> <nombreCuentaTwitch:str> [...]',
-			comando: '+twitch'
+			runIn: ['text'],
+            permissionLevel: 6,
+            guarded: true,
+			subcommands: true,
+			description: 'Añade a un streamer',
+			extendedHelp: '+twitch añadir/quitar <@StartForKiller#8184> <StartForKiller>',
+			usage: '<añadir|quitar|lista> [usuario:user] [nombreCuentaTwitch:str]',
+            comando: '+twitch',
+            usageDelim: ' '
 		});
 	}
 
-	async añadir(msg, [usuario, ...nombreCuentaTwitch]) {
-		const { errors, updated } = await msg.guild.configs.update('streamers', nombreCuentaTwitch.join(' '), msg.guild, { avoidUnconfigurable: true, action: 'add' });
-		if (errors.length) return msg.send('Ha ocurrido un error');
-		if (!updated.length) return msg.send('Ha ocurrido un error');
-		await msg.guild.members.get(usuario).roles.add(msg.guild.roles.get('407255950232256521'));
-		return msg.send('Actualizado');
+	async añadir(msg, [usuario, nombreCuentaTwitch]) {
+		const r = this.client.providers.default.db;
+		await r.table('streamers').insert({ id: usuario.id, nombreCuentaTwitch: nombreCuentaTwitch });
+		return msg.send(`<:tic:408639986934480908> La cuenta _${nombreCuentaTwitch}_ ha sido agregada correctamente a nuestra base de datos. ${usuario} ha recibido el rol de Streamer. Ahora cada vez que retransmita un vídeo de Sea of Thieves se publicará en <#407286482554847242>.`);
 	}
 
 	async quitar(msg, [usuario, ...nombreCuentaTwitch]) {
-		const { errors, updated } = await msg.guild.configs.update('streamers', nombreCuentaTwitch.join(' '), msg.guild, { avoidUnconfigurable: true, action: 'remove' });
-		if (errors.length) return msg.send('Ha ocurrido un error');
-		if (!updated.length) return msg.send('Ha ocurrido un error');
-		await msg.guild.members.get(usuario).roles.remove(msg.guild.roles.get('407255950232256521'));
-		return msg.send('Actualizado');
+		const r = this.client.providers.default.db;
+		await r.table('streamers').get(usuario.id).delete();
+		return msg.send(`<:no:432891007366070272> La cuenta _${nombreCuentaTwitch}_ ha sido eliminada de nuestra base de datos y ${usuario} ahora ya no es Streamer.`);
+	}
+
+	async lista(msg) {
+		const r = this.client.providers.default.db;
+		const tabla = await r.table('streamers');
+		let embed = new MessageEmbed().setTitle('<:twitch:473533904457039872> Lista de streamers')
+						.setColor(8478656);
+		for(let i = 0; i < tabla.length; i++) {
+			let usuario = await msg.guild.members.get(tabla[i].id);
+			if (i === 0) {
+				embed
+				.addField('Cuenta:', `${i + 1}. ${tabla[i].nombreCuentaTwitch}`, true)
+				.addField('Usuario:', usuario.displayName, true);
+			} else {
+				embed
+				.addField('\u200B', `${i + 1}. ${tabla[i].nombreCuentaTwitch}`, true)
+				.addField('\u200B', usuario.displayName, true);
+			}
+						
+		}
+		return msg.send(embed);
 	}
 
 };
--- src/tasks/twitch.js
+++ src/tasks/twitch.js
@@ -14,44 +14,67 @@ module.exports = class extends Task {
 		const guild = this.client.guilds.get(debug ? '420911335187152909' : '375828283184513033');
 		if (!guild) return false;
 
+		const r = this.client.providers.default.db;
+
 		const canal = guild.channels.get(guild.configs.channels.twitch);
-		const usuarios = guild.configs.streamers;
+		const usuarios = await r.table('streamers');
 
 		if (usuarios.length === 0 || !canal) return false;
 
-		const channels = `,${usuarios.join(',')}`;
+		let channels = '';
+
+		for (let i = 0; i < usuarios.length; i++) {
+			channels += `,${usuarios[i].nombreCuentaTwitch}`;
+		}
+		console.log(channels);
 		url.search = new URLSearchParams([['channel', channels], ['stream_type', 'live'], ['game', 'Sea of Thieves'], ['client_id', 'radjtg8zklal4pt79x0xdxfucsk61z']]);
 		const result = await fetch(url);
 		const res = await result.json();
 
-		const r = this.client.providers.default.db;
-
 		for (let i = 0; i < res.streams.length; i++) {
 			if (res.streams[i].game === 'Sea of Thieves') {
-				const createdAt = await r.table('streamers').get(res.streams[i]._id)('createdAt').default(-1);
+				const createdAt = await r.table('streams').get(res.streams[i]._id)('createdAt').default(-1);
 				if (createdAt !== -1) {
-					if (await r.table('streamers').get(res.streams[i]._id)('createdAt') !== res.streams[i].created_at) {
+					if (await r.table('streams').get(res.streams[i]._id)('createdAt') !== res.streams[i].created_at) {
+						const tabla = await r.table('streamers');
+						let imagen;
+						for (let j = 0; j < tabla.length; j++) {
+							if(tabla[i].nombreCuentaTwitch === res.streams[i].channel.display_name) {
+								imagen = await guild.members.get(tabla[i].id);
+								imagen = imagen.user.avatarURL();
+							}
+						}
+						
 						const embed = new MessageEmbed().setTitle(res.streams[i].channel.status)
-							.setAuthor(res.streams[i].channel.display_name, res.streams[i].channel.logo)
+							.setAuthor(res.streams[i].channel.display_name, imagen)
 							.setThumbnail(res.streams[i].channel.logo)
 							.setImage(res.streams[i].preview.large)
 							.addField('Game:', res.streams[i].game, true)
 							.addField('Viewers:', res.streams[i].viewers, true)
-							.setColor(7358401);
+							.setColor(8478656);
 						canal.send(embed);
 
-						await r.table('streamers').get(res.streams[i]._id).update({ id: res.streams[i]._id, createdAt: res.streams[i].created_at });
+						await r.table('streams').get(res.streams[i]._id).update({ id: res.streams[i]._id, createdAt: res.streams[i].created_at });
 					}
 				} else {
+					const tabla = await r.table('streamers');
+					let imagen;
+					for (let j = 0; j < tabla.length; j++) {
+						if(tabla[i].nombreCuentaTwitch === res.streams[i].channel.display_name) {
+							imagen = await guild.members.get(tabla[i].id);
+							imagen = imagen.user.avatarURL();
+						}
+					}
+
 					const embed = new MessageEmbed().setTitle(res.streams[i].channel.status)
-						.setAuthor(res.streams[i].channel.display_name, res.streams[i].channel.logo)
+						.setAuthor(res.streams[i].channel.display_name, imagen)
 						.setThumbnail(res.streams[i].channel.logo)
 						.setImage(res.streams[i].preview.large)
-						.addField('Game:', res.streams[i].game, true)
-						.addField('Viewers:', res.streams[i].viewers, true)
-						.setColor(7358401);
+						.addField('Juego:', res.streams[i].game, true)
+						.addField('Seguidores:', res.streams[i].channel.followers, true)
+						.setColor(8478656);
 					canal.send(embed);
-					await r.table('streamers').insert({ id: res.streams[i]._id, createdAt: res.streams[i].created_at });
+					await r.table('streams').insert({ id: res.streams[i]._id, createdAt: res.streams[i].created_at });
 				}
 			}
 		}
