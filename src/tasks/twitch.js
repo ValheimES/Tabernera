fae15d48d2939365be08814385681b2550b6dc79
kyraNET <kyradiscord@gmail.com>
2018-08-03 10:28:06 +0200

    improvements

--- src/tasks/twitch.js
+++ src/tasks/twitch.js
@@ -7,7 +7,6 @@ const fetch = require('node-fetch');
 module.exports = class extends Task {
 
 	async run() {
-		const url = new URL('https://api.twitch.tv/kraken/streams/');
 		const guild = this.client.guilds.get('420911335187152909');
 		if (!guild) return false;
 
@@ -16,17 +15,20 @@ module.exports = class extends Task {
 		const canal = guild.channels.get(guild.configs.channels.twitch);
 		if (!canal) return false;
 
-		const usuarios = await r.table('streamers');
-		if (!usuarios.length) return false;
+		const streamers = await r.table('streamers').run();
+		if (!streamers.length) return false;
 
-		const channels = usuarios.map(usuario => usuario.nombreCuentaTwitch).join(',');
+		const channels = streamers.map(usuario => usuario.nombreCuentaTwitch).join(',');
 
-		url.search = new URLSearchParams([['channel', channels], ['stream_type', 'live'], ['game', 'Sea of Thieves'], ['client_id', 'radjtg8zklal4pt79x0xdxfucsk61z']]);
+		const url = new URL('https://api.twitch.tv/kraken/streams/');
+		url.searchParams.append('channel', channels);
+		url.searchParams.append('stream_type', 'live');
+		url.searchParams.append('game', 'Sea of Thieves');
+		url.searchParams.append('client_id', 'radjtg8zklal4pt79x0xdxfucsk61z');
 		const body = await fetch(url).then(result => result.json());
 
 		for (const stream of body.streams) {
-			let entry = await r.table('streams').get(stream.id);
-			const streamers = await r.table('streamers');
+			let entry = await r.table('streams').get(stream.id).run();
 			let nuevo = false;
 			if (!entry) {
 				nuevo = true;
